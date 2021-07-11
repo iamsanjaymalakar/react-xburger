2daf90e19d8137a2e6b193f5efc45de541c2616a
Sanjay Malakar <iamsanjaymalakar@gmail.com>
2021-07-11 15:17:54 +0600

    converted Modal to funtional component.

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -1,29 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.module.css';
 
 
-class Modal extends Component {
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.show !== this.props.show
-            || nextProps.children !== this.props.children;
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-                <div className={classes.Modal}
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }}>
-                    {this.props.children}
-                </div>
-            </React.Fragment>
-        );
-    }
+const modal = props => {
+    return (
+        <React.Fragment>
+            <Backdrop show={props.show} clicked={props.modalClosed} />
+            <div className={classes.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                {props.children}
+            </div>
+        </React.Fragment>
+    );
 }
 
 
-export default Modal;
+export default React.memo(modal, (prevProps, nextProps) => {
+    return prevProps.show === nextProps.show && prevProps.children === nextProps.children;
+});
